a4003d5a235359f14a12ff306e2c1218212617c5
jsebah28 <106888673+jsebah28@users.noreply.github.com>
2023-03-15 13:56:59 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+function hideMenu() {
+    document.getElementById('menu_mobile_version').style.display = 'none';
+}
+
+function showMenu() {
+    document.getElementById('menu_mobile_version').style.display = 'block';
+}
+
+function showAskCab() {
+    document.getElementById('ask_cab').style.display = 'block';
+}
+
+function hideAskCab() {
+    document.getElementById('ask_cab').style.display = 'none';
+}
+
+function showEmpresas() {
+    document.getElementById('empresas').style.display = 'block';
+}
+
+
+
+
+
