b332930a18ec8af6e5d02d88138f9eec944fb6e8
jsebah28 <106888673+jsebah28@users.noreply.github.com>
2023-03-15 15:17:34 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -19,6 +19,22 @@ function showEmpresas() {
     document.getElementById('empresas').style.display = 'block';
 }
 
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slider img');
+const maxSlides = slides.length;
+
+function showSlide(n) {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (n + maxSlides) % maxSlides;
+  slides[currentSlide].classList.add('active');
+}
+
+function nextSlide() {
+  showSlide(currentSlide + 1);
+}
+
+setInterval(nextSlide, 2000);
+
 
 
 
